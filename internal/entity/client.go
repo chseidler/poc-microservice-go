930d43a7e7b8f6d18f562460e20f8e043722d429
Christian Seidler <ch.cseidler@gmail.com>
2023-08-12 19:23:11 -0300

    Update method for client

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -41,3 +41,15 @@ func (c *Client) Validate() error {
 
 	return nil
 }
+
+func (c *Client) Update(name string, email string) error {
+	c.Name = name
+	c.Email = email
+	c.UpdateAt = time.Now()
+
+	err := c.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -20,3 +20,19 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("John Doe Update", "g@j.com")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Update", client.Name)
+	assert.Equal(t, "g@j.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("", "g@j.com")
+
+	assert.Error(t, err, "Name is required")
+}
